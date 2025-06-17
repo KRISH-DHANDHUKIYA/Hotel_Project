a25e84eb2fd2d76befd7df800f24519318e13cb4
krish <dhandhukiya482.com>
2025-06-17 10:12:05 +0530

    ug6

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,20 +1,12 @@
-import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 
 const Blog = () => {
     return (
-        <div className="pt-5">
+        <div className="page-content1">
             <Container>
-
-                {/* Locker Room */}
                 <Row className="align-items-center mb-5">
                     <Col lg={5}>
-                        <Image
-                            src="https://mjproductsco.com/wp-content/uploads/2023/08/iStock-1364577495_reduced.jpg"
-                            alt="Locker Room"
-                            fluid
-                            rounded
-                        />
+                        <Image src="https://mjproductsco.com/wp-content/uploads/2023/08/iStock-1364577495_reduced.jpg" alt="Locker Room" fluid rounded />
                     </Col>
                     <Col lg={7}>
                         <h6 className="text-primary">Locker Room</h6>
@@ -25,15 +17,9 @@ const Blog = () => {
                     </Col>
                 </Row>
 
-                {/* Restaurant */}
                 <Row className="align-items-center mb-5 flex-lg-row-reverse">
                     <Col lg={5}>
-                        <Image
-                            src="https://s0.rbk.ru/v6_top_pics/media/img/9/39/756808723403399.jpg"
-                            alt="Restaurant"
-                            fluid
-                            rounded
-                        />
+                        <Image src="https://s0.rbk.ru/v6_top_pics/media/img/9/39/756808723403399.jpg" alt="Restaurant" fluid rounded />
                     </Col>
                     <Col lg={7}>
                         <h6 className="text-primary">Restaurant</h6>
@@ -44,15 +30,9 @@ const Blog = () => {
                     </Col>
                 </Row>
 
-                {/* Car Parking */}
                 <Row className="align-items-center mb-5">
                     <Col lg={5}>
-                        <Image
-                            src="https://images.unsplash.com/photo-1616363088386-31c4a8414858"
-                            alt="Car Parking Area"
-                            fluid
-                            rounded
-                        />
+                        <Image src="https://images.unsplash.com/photo-1616363088386-31c4a8414858" alt="Car Parking Area" fluid rounded />
                     </Col>
                     <Col lg={7}>
                         <h6 className="text-primary">Car Parking</h6>
@@ -63,15 +43,9 @@ const Blog = () => {
                     </Col>
                 </Row>
 
-                {/* Gym Center */}
                 <Row className="align-items-center mb-5 flex-lg-row-reverse">
                     <Col lg={5}>
-                        <Image
-                            src="https://architizer-prod.imgix.net/media/mediadata/uploads/1554276184209BJK10.jpg"
-                            alt="Gym Center"
-                            fluid
-                            rounded
-                        />
+                        <Image src="https://architizer-prod.imgix.net/media/mediadata/uploads/1554276184209BJK10.jpg" alt="Gym Center" fluid rounded />
                     </Col>
                     <Col lg={7}>
                         <h6 className="text-primary">Gym Center</h6>
@@ -82,15 +56,9 @@ const Blog = () => {
                     </Col>
                 </Row>
 
-                {/* Swimming Pool */}
                 <Row className="align-items-center mb-5">
                     <Col lg={5}>
-                        <Image
-                            src="https://3.bp.blogspot.com/-hqp-nmJVzH0/WmhTL53kQtI/AAAAAAAAAAM/nrb0Ni-W_gkUmSDxS4x_7gXiBF568QxEACLcBGAs/s1600/Swimming%2BPool%2BConstruction%2BAdelaide.jpg"
-                            alt="Swimming Pool"
-                            fluid
-                            rounded
-                        />
+                        <Image src="https://3.bp.blogspot.com/-hqp-nmJVzH0/WmhTL53kQtI/AAAAAAAAAAM/nrb0Ni-W_gkUmSDxS4x_7gXiBF568QxEACLcBGAs/s1600/Swimming%2BPool%2BConstruction%2BAdelaide.jpg" alt="Swimming Pool" fluid rounded />
                     </Col>
                     <Col lg={7}>
                         <h6 className="text-primary">Swimming Pool</h6>
@@ -100,7 +68,6 @@ const Blog = () => {
                         </p>
                     </Col>
                 </Row>
-
             </Container>
         </div>
     );
